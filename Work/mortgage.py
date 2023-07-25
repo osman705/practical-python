041b9a1319e7f1b0fec43525a3556694292ef0f5
Osman H <ohassan705@gmail.com>
2023-07-25 17:16:38 +0100

    Ez

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,24 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+
+while principal > 0:
+    if months < 12:
+       principal = principal * (1+rate/12) - (payment + 1000)
+       total_paid = total_paid + (payment + 1000)
+       months += 1
+    else:
+        principal = principal * (1+rate/12) - (payment)
+        total_paid = total_paid + payment
+        months += 1
+
+print(months)
+print('Total paid', total_paid)
+
